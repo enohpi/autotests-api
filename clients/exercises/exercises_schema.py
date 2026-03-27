873d76ca56614d47ec0be07a7f1519caf61a62fa
enohpi <vadim-7210@yandex.ru>
2026-03-27 19:55:06 +0700

    7.2 Применяем Pydantic в API клиентах Практикуемся в применении Pydantic в API клиентах

---
+++ clients/exercises/exercises_schema.py
@@ -0,0 +1,83 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class ExerciseSchema(BaseModel):
+    """
+    Описание структуры задания.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: str
+    title: str
+    course_id: str = Field(alias="courseId")
+    max_score: int = Field(alias="maxScore")
+    min_score: int = Field(alias="minScore")
+    order_index: int = Field(alias="orderIndex")
+    description: str
+    estimated_time: str = Field(alias="estimatedTime")
+
+
+class GetExerciseResponseSchema(BaseModel):
+    """
+    Описание структуры запроса на получения задания.
+    """
+    exercise: ExerciseSchema
+
+
+class GetExercisesResponseSchema(BaseModel):
+    """
+    Описание структуры задания.
+    """
+    exercises: list[ExerciseSchema]
+
+
+class GetExercisesQuerySchema(BaseModel):
+    """
+    Описание структуры запроса на получение списка заданий.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    courseId: str
+
+
+class CreateExerciseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на создание задания.
+    """
+    exercise: ExerciseSchema
+
+
+class UpdateExerciseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления задания.
+    """
+    exercise: ExerciseSchema
+
+
+class CreateExerciseRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание задания.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    title: str
+    course_id: str = Field(alias="courseId")
+    max_score: int = Field(alias="maxScore")
+    min_score: int = Field(alias="minScore")
+    order_index: int = Field(alias="orderIndex")
+    description: str
+    estimated_time: str = Field(alias="estimatedTime")
+
+
+class UpdateExerciseRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на обновление задания.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    title: str | None
+    max_score: int | None = Field(alias="maxScore")
+    min_score: int | None = Field(alias="minScore")
+    order_index: int | None = Field(alias="orderIndex")
+    description: str | None
+    estimated_time: str | None = Field(alias="estimatedTime")
